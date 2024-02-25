4d70450d5155fc50bc82ff581c69251525796e2e
sparshbarolia <107509768+sparshbarolia@users.noreply.github.com>
2024-02-26 01:27:05 +0530

    removed extra api

--- index.js
+++ index.js
@@ -6,26 +6,6 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-const UserDB = require("./connection")
-app.get("/all" , (req, res) => {
-
-  const query = 'SELECT * FROM userScore WHERE WEEK(TimeStamp)=WEEK(CURRENT_TIMESTAMP) ORDER BY score desc';
-
-  try{
-      UserDB.query(query, (error, results, fields) => {
-          if (error) {
-              console.error('Error executing query: ' + error.stack);
-              return res.status(500).json({ error: 'Internal server error' });
-          }
-          res.json(results);
-      });
-  }
-  catch(err){
-      console.error('Error executing query: ' + error.stack);
-      return res.status(500).json({ error: 'Internal server error' });
-  }
-})
-
 app.use("/api",userRoutes);
 
 app.listen(PORT,() => {
