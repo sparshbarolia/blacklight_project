e2aceee929beb1f757fcfb89e3ba41fb31c82d15
sparshbarolia <107509768+sparshbarolia@users.noreply.github.com>
2024-02-26 15:20:58 +0530

    views folder added

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 
 const UserDB = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
+    host: process.env.SQLHOST,
     user: process.env.SQLUSER,
     password: process.env.SQLPASSWORD,
     database: 'blacklight',
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+app.get("/",(req,res) => {
+    res.render('home');
+})
 
 app.use("/api",userRoutes);
 
