80efdc21fe976d1db9fd5bf902a74e918b8ba719
sparshbarolia <107509768+sparshbarolia@users.noreply.github.com>
2024-02-26 11:26:19 +0530

    deployment setup

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,13 @@ const {getCurrentWeekLeaderboard , getLastWeekCountryLeaderboard , getRankByID} 
 
 const router = express.Router();
 
-
+// Display current week leaderboard (Top 200)
 router.get('/leaderboard/currentweek', getCurrentWeekLeaderboard);
 
+// Display last week leaderboard given a country by the user (Top 200)
 router.get('/leaderboard/lastweek/:country', getLastWeekCountryLeaderboard);
 
+// Fetch user rank, given the userId
 router.get('/rank/currentweek/:userid', getRankByID);
 
 module.exports = router;
